9227d335455b8f26a256dd25907cbb269ec43294
FrankEberhard <spam@frankeberhard.com>
2019-10-17 19:12:26 +0200

    Timer Class extended. GUI implementation started. Not functional

---
+++ GUI.py
@@ -0,0 +1,72 @@
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtCore import (Qt, pyqtSignal, QObject)
+from PyQt5.QtGui import QIcon, QPixmap
+import sys
+
+
+# create Signals
+class CallbackObject(QObject):
+    signal_info = pyqtSignal(str, name='SIGNAL_INFO')
+    signal_lcd = pyqtSignal(int, name='SIGNAL_LCD')
+
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.triggerSignal()
+
+    def triggerSignal(self):
+        self.signal_info.emit('SIGNAL_INFO')
+        print('Info Triggered!')
+        self.signal_lcd.emit('SIGNAL_LCD')
+        print('LCD Triggered!')
+
+
+# UI Class
+class UiIotaPay(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super(UiIotaPay, self).__init__(parent)
+        uic.loadUi('./IotaPayV001.ui', self)
+
+        self.threader = UiThreads()
+        self.threader.start()
+
+        self.info_label = self.findChild(QtWidgets.QLabel, 'info_lbl')
+        self.lcd_ui = self.findChild(QtWidgets.QLCDNumber, 'lcd_nr')
+
+        # create CallbackObject and connect signals to slots
+        self._callback = CallbackObject(parent=parent)
+        self._callback.signal_info.connect(UiThreads)
+        self._callback.signal_lcd.connect(UiThreads)
+
+        self.updateUi()
+
+    # update the UI
+    def updateUi(self):
+        self.info_label.setText(IotaPay.info)
+        print('push Info')
+        self.lcd_ui.display(IotaPay.txValue)
+        print('push LCD')
+
+
+# Thread Class
+class UiThreads(QtCore.QThread):
+    def __init__(self, parent=None):
+        super(UiThreads, self).__init__(parent)
+
+    def run(self):
+
+        while 1:
+            info = IotaPay.info
+            lcd = IotaPay.txValue
+            app._callback.triggerSignal()
+            #CallbackObject.signal_info.emit('SIGNAL_INFO')
+            #self.updater(self)
+            IotaPay.txValue = IotaPay.txValue + 1
+            print('threads running', IotaPay.txValue)
+            time.sleep(0.5)
+
+
+# a = QtWidgets.QApplication(sys.argv)
+# app = UiIotaPay()
+# app.show()
+# sys.exit(a.exec())
--- Timer.py
+++ Timer.py
@@ -4,19 +4,36 @@ import time
 class Timer:
     def __init__(self, stop):
 
-        self.stop = stop
+        self.stop = int(stop / 10) # implement in Configuration (or extra class) to set up price iota/second
 
     def timer(self):
 
         while True:
             while self.stop >= 0:
-                m, s = divmod(self.stop, 60)
-                h, m = divmod(m, 60)
-                time_left = str(h).zfill(2) + ':' + str(s).zfill(2)
-                print(time_left + "\r")
+                s = self.stop
+                time_left = str(s).zfill(4)
+                print(time_left + '\r')
                 time.sleep(1)
                 self.stop -= 1
                 if self.stop <= 0:
                     print('out of time!')
                     break
             break
+
+    def timer_load(self):
+
+        self.start = 0
+
+        while True:
+            while self.start <= self.stop:
+                s = self.start
+                time_set = str(s).zfill(4)
+                time.sleep(0.1)
+                self.start += 1
+                print(time_set)
+
+            break
+        time.sleep(1)
+        print('STARTING TIMER')
+        time.sleep(2)
+        self.timer()
